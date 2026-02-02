7e9b6b12bd6511f0cab1bb8a3590b42d08f93702
Shenori Ruwesha <shenoriperera87@gmail.com>
2026-02-02 16:54:59 +0530

    Add About jsx file and css file

---
+++ Profolio-react/src/Components/About/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './About.css';
+
+const About = () => {
+  return (
+    <div className='about'>
+        <div className='about-title'>
+            <h1>About Me</h1>
+        </div>
+      
+    </div>
+  )
+}
+
+export default About
