0047ca4c4e4871090a240b0a73979fcdb63a9264
Shenori Ruwesha <shenoriperera87@gmail.com>
2026-02-11 11:14:11 +0530

    Add some properties and services folder

---
+++ Profolio-react/src/Components/Services/Services.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Services.css';
+
+const Services = () => {
+  return (
+    <div className='services'>
+      <div className='services-title'>
+        <h1>My Services</h1>
+        <img src={" "} alt=""/>
+      </div>
+      <div className='services-container'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Services
